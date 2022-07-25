caba1b2735dd8b26211307c792f71760d531c65b
ayelitava <cveaen@gmail.com>
2022-07-25 15:33:01 +0300

    new dz5

---
+++ dz5.py
@@ -0,0 +1,89 @@
+# i = 1 
+# while i < 7: 
+#     print('I LOVE PYTHON') 
+
+
+
+# a = 1 
+# while a < 7: 
+#     a += 1 
+#     if a == 3: 
+#         break
+#     print('I LOVE PYTHON')
+
+
+
+
+# list_names = ['Aza', 'Kuma', 'Bama', 'Anna', 'Vika']
+# get_names = ['Kuma', 'Anna', 'Adilet', 'Sasha'] 
+# new = list (set(list_names+get_names))
+# print(new) 
+
+
+# def Hello (): 
+#     print("Hello americans")
+
+# def Bay (): 
+#     print("Hello chines")
+
+# Hello()
+# Bay()
+
+
+
+
+# def Hello2 (mes):
+#     print("Hello", mes)
+
+# Hello2(input("Введите текст: "))
+
+
+# x = -42 
+# abs_x = abs(x)
+# print(f"Абсолютное значение {x} это {abs_x}")
+
+# def isPositive(x): 
+#      if x >= 0: 
+#         return True 
+#      else: 
+#         return False 
+
+# a = [3,4, -4, 4, -323, 323, -32, 12, -4, 12]
+# p = []
+# for i in a: 
+#     if isPositive(i): 
+#         p.append(i)
+# print(p)
+
+
+
+
+
+
+
+
+# while True:
+#     s = input("знак (+,-,*,/): ") 
+#     if s == '0' : 
+#         break
+#     if s in ('+', '-', '*', '/'): 
+#         x = float(input("x="))
+#         y = float(input("y="))
+#         if s == '+': 
+#             print("%.2f" % (x+y))
+#         elif s == '-':
+#             print("%.2f" % (x-y))
+#         elif s == '*':
+#             print("%.2f" % (x*y))
+#         elif s == '/': 
+#             if y != 0: 
+#                 print("%.2f" % (x/y))
+#             else: 
+#                 print("Деление на ноль!")
+#     else:
+     
+#      print("Неверный знак операции!")
+
+
+
+
